282015e9d1a3315daa39aa1128a141d9e0d1dbc7
shakhawatsh <shakhawatshuvo1@gmail.com>
2024-05-01 12:46:48 +0600

    master push

---
+++ lib/data.dart
@@ -0,0 +1,9 @@
+
+import 'package:flutter/material.dart';
+class Data{
+  String image;
+  String text;
+
+  Data({required this.image, required this.text}
+      );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,46 @@
-void main(){
-  
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+import 'home_land.dart';
+import 'home_port.dart';
+
+void main() {
+  runApp(
+    DevicePreview(
+      enabled: true,
+      builder: (context) => const MyApp(), // Wrap your app
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: OrientationBuilder(
+        builder: (context, orientation) {
+          if (orientation == Orientation.landscape) {
+            return HomeLand();
+          } else {
+            return HomePort();
+          }
+        },
+      ),
+    );
+  }
 }
