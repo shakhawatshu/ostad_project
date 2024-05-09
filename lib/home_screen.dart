3b77d0cf16d12b3b07deebc17f17dede7396f39c
shakhawatsh <shakhawatshuvo1@gmail.com>
2024-05-10 00:21:34 +0600

    WaterTrackerApp project UI+basic funtion

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -11,7 +8,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final TextEditingController _glassInputCountTEController= TextEditingController(text:'1');
-  int _count= 0;
+  List<WaterConsume> waterConsumeList=[];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,12 +26,12 @@ class _HomeScreenState extends State<HomeScreen> {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Text('Today\'s Water Consume count: $_count', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                  Text('Today\'s Water Consume count:${_totalGlassCount()}', style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
                   const SizedBox(height: 12,),
                   const Text('Tap here to Count Water Consume', style: TextStyle(fontSize: 18),),
                   const SizedBox(height: 12,),
                   GestureDetector(
-                    onTap: (){},
+                    onTap: _addWaterConsume,
                     child: Container(
                       decoration: BoxDecoration(shape: BoxShape.circle,
                         border: Border.all(color: Colors.amberAccent,width: 10,)
@@ -51,6 +48,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     child: TextField(
                       textAlign: TextAlign.center,
                       controller: _glassInputCountTEController,
+                      keyboardType: TextInputType.number,
                     ),
                   ),
                 ],
@@ -64,14 +62,14 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
             Expanded(
               child: ListView.builder(
-                itemCount: 10,
+                itemCount: waterConsumeList.length,
                 itemBuilder: (context, index) {
                 return ListTile(
-                  title: Text('Time'),
+                  title: Text(waterConsumeList[index].time.toString()),
                   leading: CircleAvatar(
                     child: Text('${index+1}'),
                   ),
-                  trailing: Text('1'),
+                  trailing: Text(waterConsumeList[index].glassCount.toString()),
                 );
               },),
             )
@@ -80,4 +78,32 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
+  //Setter Method(cant return)
+  void _addWaterConsume(){
+    int glassCount= int.tryParse(_glassInputCountTEController.text)?? 1;
+    WaterConsume waterConsume= WaterConsume(time: DateTime.now(), glassCount: glassCount);
+    waterConsumeList.add(waterConsume);
+    setState(() {
+
+    });
+
+  }
+  //Getter Method (can return)
+  int _totalGlassCount(){
+    int totalCount = 0;
+    for(WaterConsume waterConsume in waterConsumeList){
+      totalCount += waterConsume.glassCount;
+    }
+    return totalCount;
+  }
+
+
+}
+
+class WaterConsume{
+
+  final DateTime time;
+  final int glassCount;
+
+  WaterConsume({required this.time, required this.glassCount});
 }
